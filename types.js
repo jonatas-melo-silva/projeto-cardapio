ae1becf400d7ef63dda2e17e60dce4c1f61c1be3
Jonatas Melo Silva <jonatas891024@gmail.com>
2024-03-12 16:11:50 -0300

    feat: adiciona scripts

---
+++ types.js
@@ -0,0 +1,24 @@
+/**
+ * @typedef {Object} CartItem
+ * @property {string} name
+ * @property {number} price
+ * @property {number} quantity
+ */
+
+/**
+ * @typedef {Object} Cart
+ * @property {CartItem[]} items
+ * @property {number} total
+ * @property {number} count
+ */
+
+/**
+ * @typedef {Object} IncomingItem
+ * @property {string} name
+ * @property {number} price
+ */
+
+/**
+ * @typedef {Object} OncomingItem
+ * @property {string} name
+ */
